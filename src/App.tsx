337de5fd23ec2e0d99f38fced042b87e5c060f72
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-05 20:47:01 +0530

    successfully implemented manual data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import contents from './utils/contents';
 
 function App() {
-  return (
-    <div className="App h-screen bg-slate-900 ">
-      <Navbar />
-      <div className='flex flex-col justify-center items-center pt-5 mt-10  '>
-        <Card />
+  const cards = contents.map((data) => {
+    return(
+      <>
+        <Card key={data.id} {...data}/>
         <hr className='border-gray-600 m-6 w-[52rem]' />
+      </>
+    )
+    })
+
+  return (
+    <div className="App bg-slate-900 ">
+      <Navbar />
+      <div className='flex flex-col justify-center items-center pt-3 mt-5 '>
+        {cards}        
       </div>
     </div>
   );
---
+++ src/utils/contents.tsx
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        country: "Japan",
+        fromDate: "12 Jan, 2021",
+        tillDate: "24 Jan, 2021",
+        place: "Mount Fuji",
+        img:"https://source.unsplash.com/WLxQvbMyfas",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    },
+    {
+        id: 2,
+        country: "Australia",
+        fromDate: "27 May, 2021",
+        tillDate: "8 Jun, 2021",
+        place: "Sydney Opera House",
+        img:"https://source.unsplash.com/JmuyB_LibRo",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    },
+    {
+        id: 3,
+        country: "Norway",
+        fromDate: "1 Oct, 2021",
+        tillDate: "18 Nov, 2021",
+        place: "Geirangerfjord",
+        img:"https://source.unsplash.com/3PeSjpLVtLg",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    },
+]
+
